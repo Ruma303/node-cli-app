4c82ad7277d623e227ad6e81f6b303e077ee0811
Ruma303 <matteoh8@hotmail.it>
2023-10-25 09:52:31 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//% chalk
+import chalk from 'chalk';
+console.log(chalk.red('Testo'));
+
+
+//% Process
+/*
+console.log(process)
+
+process.argv.forEach(
+    function (val, index, array) {
+        console.log(index + ' : ' + val)
+    }
+); */
