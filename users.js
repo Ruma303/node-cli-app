234fb3cb177b3446411fd1deae7de90fdbac49b2
Ruma303 <matteoh8@hotmail.it>
2023-10-26 13:15:36 +0200

    modifiche

--- users.js
+++ users.js
@@ -2,11 +2,10 @@
 
     function getUser(name) {
         const usersJSON = fs.readFileSync('users.json', 'utf-8');
-        const users = JSON.parse(usersJSON);
-        const user = users.users.find(userName => userName.name === name);
+              usersParsed = JSON.parse(usersJSON);
+              user = usersParsed.users.find(userName => userName.name === name);
         return user;
     }
 
-    module.exports = {
-        getUser
-    };
+    module.exports = { getUser };
+
